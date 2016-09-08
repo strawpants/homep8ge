fc8ab0bb46ab6a59452c0a4e71d8d7d43773ea04
Roelof Rietbroek (@pretslet) <r.rietje@gmail.com>
2016-09-08 22:54:47 +0200

    better parsing of zotero entries

--- Makefile
+++ Makefile
@@ -12,7 +12,7 @@ show:
 	hugo server --baseURL public/
 #make an automated publication list
 pubs:
-	tools/mkPublist.py
+	tools/mkPublist.py /home/roelof/wolk7/zoteroData/storage 3253202 Rietbroek 'Theses;RRthesis' 'Peer Reviewed Articles;RRpeer'  > layouts/partials/pubsbody.html
 
 clean:
 	cd public; rm -rf 404.html  about  categories  css  favicon.png  fonts  images  index.html  index.xml  js  post  project  sitemap.xml  tags publications
--- tools/mkPublist.py
+++ tools/mkPublist.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 #python script to parse publications from zotero in html for my academic homepage
+# the produced html (written to stdout) is intended to be used as a partial layout for the hugo generated webpage
 # aug 2016, Roelof Rietbroek
+# This script is called in the following way:
+# mkPubList.py STORAGESUBDIROFZOTERO ZOTEROUSERID AUTHORNAMETOHIGHLIGHT 'SECTIONTITLE;ZOTEROTAG' 'SECTIONTITLE2;ZOTEROTAG2' ..
+
 
 from pyzotero import zotero
 import keyring
@@ -9,91 +13,102 @@ import shutil
 import re
 import codecs
 import datetime
+import sys
 
-def print_bibitem(item,zotinstance,fid,style,authorre,divre,doire):
+#global counter for tooltip references
+tt=0
+
+#def print_bibitem(item,zotinstance,fid,style,authorre,divre,doire):
         
 
+def main(args):
+    #get some variables from command line arguments (first argumetn is root directory of the zotero storage
+    zoterodatdir=args[1]
+    library_id=args[2]
+    authorhighlight=args[3]
+    
+    #get the to be parsed sections with their tags (format of the  argument is: sectiontitle;zoterotag)
+    sections=args[4:]
+    library_type='user'
+    
 
-#set up some global variables
-zoterodatdir='/home/roelof/wolk7/zoteroData/storage'
-root='/home/roelof/work2/homep8ge'
-library_id='3253202'
-library_type='user'
-citestyle='geophysical-research-letters'
-#to put your APIkey in your keyring adapt the following:
-#keyring.set_password('Zotero','PyzoteroKey','YOURAPIkey')
-api_key=keyring.get_password('Zotero','PyzoteroKey')
-htmlout=root+'/layouts/partials/pubsbody.html'
-author=b'Rietbroek'
-authorsearch=re.compile(author)
-div=re.compile(b'div')
-#regular expression to search for the doi (will be converted to  a link later)
-doi=re.compile(b'doi:(.+)\.<\/div>')
-zot = zotero.Zotero(library_id, library_type, api_key)
+    #to put your APIkey in your keyring adapt the following:
+    #keyring.set_password('Zotero','PyzoteroKey','YOURAPIkey')
+    api_key=keyring.get_password('Zotero','PyzoteroKey')
+   
+   
+    #get an instance of the zotero communicator
+    zot = zotero.Zotero(library_id, library_type, api_key)
+    #print to stdout
+    fid=sys.stdout.buffer
 
-fid=open(htmlout,'wb')
+#loop over the requested sections
+    for sec in sections:
+        tmp=str(sec).split(';')
+        print_bibitems(fid,zot,zoterodatdir,authorhighlight,tmp[1],tmp[0])
 
-# find Theses
-
-
-## find all peer reviewed pubs (tagged with RRpeer in zotero)
-
-fid.write(b'<div class="mdl-card__supporting-text" >\n\t<h4> Peer reviewed </h4>\n')
-fid.write(b'</div>\n')
-fid.write(b'<div class="mdl-list">\n')
-
-
-zot.add_parameters(tag='RRpeer',sort='date')
-items = zot.everything(zot.top())
-tt=0
-for item in items:
-	#get a parsed entry for each hit 
-	htmlentry=zot.item(item['key'],content='bib',style=citestyle)[0].encode('utf-8')
-	htmlentry=authorsearch.sub(b'<b>'+author+b'</b>',htmlentry)
-	
-	#replace doi by a link:
-	htmlentry=doi.sub(b'<a href="http://dx.doi.org/\\1">doi:\\1.</a></div>',htmlentry)
-	
-	htmlentry=div.sub(b'p',htmlentry)
-	#fid.write(b'\t<div class="hugo-ref-item">\n')
-	fid.write(b'\t<div class="mdl-list__item mdl-color-text--grey-700">\n')
-	
-	fid.write(b'\t\t<div class="mdl-list-primary-content">\n')
-	
-	fid.write(b'\t\t\t'+htmlentry+b'\n')
-	
-	fid.write(b'\t\t</div>\n')
-	#find out if there is a free pdf attached which we can provide a link to
-	freepdfentry=zot.children(item['key'],tag='freepdf')
-	if freepdfentry:
-		src=zoterodatdir+'/'+freepdfentry[0]['data']['key']+'/'+freepdfentry[0]['data']['filename']
-		destpdf='/data/'+freepdfentry[0]['data']['filename']
-		#copy the pdf to the static folder if there does not exist one yet
-		if not os.path.isfile(root+'/static'+destpdf):
-			shutil.copy2(src,root+'/static'+destpdf)
-
-		#modify the html entry to include the link
-		tt+=1
-		ttid=('tt'+str(tt)).encode('utf-8')
-		fid.write(b'\t\t<div class=mdl-list-secondary-content>\n')
-		
-		fid.write(b'\t\t\t<a  id='+ttid+b' class="mdl-list__item-secondary-action mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect icon-button" href="'+destpdf.encode('utf-8')+b'" aria-label="Download pdf" data-upgraded="MaterialButton,MaterialRipple"><i class="material-icons_2x icon ion-archive"></i><span class="mdl-button__ripple-container"><span class="mdl-ripple"></span></span>\n')
-		fid.write(b'\t\t\t</a>\n')
-		
-		fid.write(b'\t\t\t<div class="mdl-tooltip mdl-tooltip" for="'+ttid+b'">Free preprint\n')
-		fid.write(b'\t\t\t</div>\n')
-		
-		fid.write(b'\t\t</div>\n')
-		#pdflink=b'<div><a id='+ttid+b' class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect icon-button" href="'+destpdf.encode('utf-8')+b'" aria-label="Download pdf" data-upgraded=",MaterialButton,MaterialRipple"><i 		class="material-icons_2x icon ion-archive"></i><span class="mdl-button__ripple-container"><span class="mdl-ripple"></span></span></a><div class="mdl-tooltip mdl-tooltip" for="'+ttid+b'">Download pdf</div></div>\n'
-		#fid.write(b'\t\t'+pdflink+b'\n</div>\n')	
-	
-	fid.write(b'\t</div>\n')
-#add date tag 
-now=datetime.datetime.now()
-fid.write(b'<small> Bibliography generated from zotero by <a href="https://github.com/strawpants/homep8ge/blob/master/tools/mkPublist.py"> mkPublist.py </a> on '+now.strftime("%B %d, %Y").encode('utf-8')+b'</small>\n')
-fid.write(b'</div>\n')
+            
+    #add modification tag with date 
+    now=datetime.datetime.now()
+    fid.write(b'<div class="mdl-card__supporting-text"><small> Bibliography generated from zotero by <a href="https://github.com/strawpants/homep8ge/blob/master/tools/mkPublist.py"> mkPublist.py </a> on '+now.strftime("%b %d, %Y").encode('utf-8')+b'</small>\n</div>\n')
+    
 
 
+ 
+    
+    
+def print_bibitems(fid,zot,datadir,author,tag,name):
+    citestyle='geophysical-research-letters'
+    zot.add_parameters(tag=tag,sort='date')
+    fid.write(b'<div class="mdl-card__supporting-text" >\n\t<h4>'+str(name).encode('utf-8')+b'</h4>\n</div>\n')
+    fid.write(b'<div class="mdl-list">\n')
+    items = zot.everything(zot.top())
+    global tt;
+    for item in items:
+    #get a parsed entry for each hit 
+            htmlentry=zot.item(item['key'],content='bib',style=citestyle)[0].encode('utf-8')
+            #highlight author
+            htmlentry=re.sub(author.encode('utf-8'),b'<b>'+author.encode('utf-8')+b'</b>',htmlentry)
+           
+            #replace doi by a highlighted link:
+            htmlentry=re.sub(b'doi:(.+)\.<\/div>',b'<a href="http://dx.doi.org/\\1">doi:\\1.</a></div>',htmlentry)
+            
+            htmlentry=re.sub(b'div',b'p',htmlentry)
+            #fid.write(b'\t<div class="hugo-ref-item">\n')
+            fid.write(b'\t<div class="mdl-list__item mdl-color-text--grey-700">\n')
+            
+            fid.write(b'\t\t<div class="mdl-list-primary-content">\n')
+            
+            fid.write(b'\t\t\t'+htmlentry+b'\n')
+            
+            fid.write(b'\t\t</div>\n')
+            #find out if there is a free pdf attached which we can provide a link to
+            freepdfentry=zot.children(item['key'],tag='freepdf')
+            if freepdfentry:
+                    src=datadir+'/'+freepdfentry[0]['data']['key']+'/'+freepdfentry[0]['data']['filename']
+                    destpdf='/data/'+freepdfentry[0]['data']['filename']
+                    #copy the pdf to the static folder if there does not exist one yet
+                    if not os.path.isfile('static'+destpdf):
+                            shutil.copy2(src,'static'+destpdf)
 
-fid.close()	
+                    #modify the html entry to include the link
+                    tt+=1
+                    ttid=('tt'+str(tt)).encode('utf-8')
+                    fid.write(b'\t\t<div class=mdl-list-secondary-content>\n')
+                    
+                    fid.write(b'\t\t\t<a  id='+ttid+b' class="mdl-list__item-secondary-action mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect icon-button" href="'+destpdf.encode('utf-8')+b'" aria-label="Download pdf" data-upgraded="MaterialButton,MaterialRipple"><i class="material-icons_2x icon ion-archive"></i><span class="mdl-button__ripple-container"><span class="mdl-ripple"></span></span>\n')
+                    fid.write(b'\t\t\t</a>\n')
+                    
+                    fid.write(b'\t\t\t<div class="mdl-tooltip mdl-tooltip" for="'+ttid+b'">Download pdf\n')
+                    fid.write(b'\t\t\t</div>\n')
+                    
+                    fid.write(b'\t\t</div>\n')
+                    #pdflink=b'<div><a id='+ttid+b' class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect icon-button" href="'+destpdf.encode('utf-8')+b'" aria-label="Download pdf" data-upgraded=",MaterialButton,MaterialRipple"><i 		class="material-icons_2x icon ion-archive"></i><span class="mdl-button__ripple-container"><span class="mdl-ripple"></span></span></a><div class="mdl-tooltip mdl-tooltip" for="'+ttid+b'">Download pdf</div></div>\n'
+                    #fid.write(b'\t\t'+pdflink+b'\n</div>\n')	
+            
+            fid.write(b'\t</div>\n')
+    fid.write(b'</div>\n')    
 
+
+if __name__ == "__main__":
+    main(sys.argv)
